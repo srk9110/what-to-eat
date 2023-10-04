21b0bc79059cdeb8f7defdca4a7e6d291561b967
srk9110 <srk911028@gmail.com>
2023-10-04 16:35:53 +0900

    마커 추가

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {useState, useEffect, useRef} from 'react';
-import Script from 'next/script';
 import { useSearchParams } from 'next/navigation'
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import Link from 'next/link';
@@ -34,7 +33,6 @@ export default function Result(){
 
     const [page, setPage] = useState<number>(1);
     const [list, setList] = useState<Info[]>();
-    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     const refs = useRef<any[]>([]);
     let kakaoMapScript: HTMLScriptElement | null = null;
@@ -44,7 +42,6 @@ export default function Result(){
     },[]);
     
     useEffect(() => {
-        
         if(list && list.length) {
             if(!kakaoMapScript){
                 kakaoMapScript = document.createElement('script');
@@ -55,20 +52,23 @@ export default function Result(){
 
             const onLoadKakaoAPI = () => {
                 window.kakao.maps.load(() => {
-                    let container = null;
-                    let map = null;
+                    let container, map, markerPosition, marker = null;
 
                     list.map((item, index) => {
                         container = document.getElementById("map_"+index);
-                        map = new window.kakao.maps.Map(container, 
+                        map = new window.kakao.maps.Map(
+                            container, 
                             {
                                 center: new window.kakao.maps.LatLng(item?.y, item?.x),
                                 level: 3,
-                            })
-                        });
-
-                    })
-                }
+                            }
+                        )
+                        markerPosition  = new window.kakao.maps.LatLng(item?.y, item?.x);
+                        marker = new window.kakao.maps.Marker({position: markerPosition});
+                        marker.setMap(map);
+                    });
+                });
+            }
             
             kakaoMapScript?.addEventListener('load', onLoadKakaoAPI)
         }
@@ -172,7 +172,7 @@ export default function Result(){
                 {
                     list && list.length ? list.map((item,index) => (
                         <li ref={el => (refs.current[index] = el)} key={index} id={item.place_name} className='bg-white rounded-lg shadow-md p-4 border border-gray-100 mb-2'>
-                            <div>
+                            <div className='mb-6'>
                                 <div className='text-lg font-semibold mb-1'>{item.place_name}</div>
                                 <div className='text-sm text-gray-400 mb-3'>{item.category_name ? returnCategory(item.category_name) : "-"}</div>
                                 <div>{item.phone}</div>
@@ -180,9 +180,7 @@ export default function Result(){
                                 <a href={item.place_url} target="_blank" className='inline-block text-xs rounded-lg bg-slate-400 text-white py-1 px-2 mr-2'>상세 보기</a>
                                 <div className='inline-block text-xs text-white py-1 px-2 rounded-lg bg-orange-500' onClick={() => {}}>지도 보기</div>
                             </div>
-                            <div>
-                                <div id={`map_${index}`} className='map w-full h-80 mb-4'/>
-                            </div>
+                            <div id={`map_${index}`} className='map w-full h-60 mb-4'/>
                         </li>
                     ))
                     : null

63320924ded755e26be5925527667a0ec24cb420
srk9110 <srk911028@gmail.com>
2023-10-10 14:27:20 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: '어디서 먹을까',
+  title: '어디서 뭐 먹을까',
 }
 
 export default function RootLayout({
@@ -22,9 +22,9 @@ export default function RootLayout({
         <section className='max-w-screen-sm mx-auto bg-white'>
         <div className='flex flex-col items-center pb-12 px-4 title-wrap'>
           <h2 className='title text-3xl py-6 text-gray-700 font-normal'>
-            <Link href={"/"}>어디서 먹을까?🤔</Link>
+            <Link href={"/"}>어디서 뭐 먹을까?🤔</Link>
           </h2>
-          <div className='text-sm font-medium text-gray-500 text-center break-keep'>
+          <div className='text-gray-500 text-center break-keep text-base'>
             직장에서, 여행지에서, 약속 장소에서 어디서 식사할지 고민이신가요?<br/>
             지역과 카테고리에 따라 무작위로 골라드립니다 :)
           </div>
